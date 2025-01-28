db916680918269b4efeb8844d15f85e6bd4e40e4
Rodrigo Martinez Delgado <rodrigo.mardel.daw@gmail.com>
2025-01-28 19:32:07 +0100

    ej9 finalizado

---
+++ 2-Cliente-JavaScript/UT7_AJAX/ejercicios/ej9_localidadesPOST_FETCH+ASYNC/localidad.js
@@ -0,0 +1,61 @@
+/*
+Puedes partir de los archivos localidadGETXHR.html y arraylocalidades.php.
+Qué debe hacer la página:
+•	Cuando el usuario introduzca el nombre de una localidad y pulse intro obtendrá, en el  <p id="resultado">, un mensaje que indicará si la ciudad está incluida dentro de una lista de ciudades o no. El mensaje será rojo si no está incluida y verde en caso afirmativo.
+•	El archivo arraylocalidades.php comprueba que la localidad recibida por parámetro está o no incluida dentro de una lista de 10 localidades (ignora la diferencia entre mayúsculas y minúsculas). Devuelve “S” si está incluida y “N” si no está incluida.
+
+** EXT: utiliza promesas en lugar de  XMLHttpRequest.
+*/
+let file, localidadUsuario;
+
+document.addEventListener("DOMContentLoaded", inicio);
+
+/** Carga inicial despúes del DOM*/
+function inicio() {
+  localidadUsuario = document.getElementById("localidad");
+
+  file = new URL("./arraylocalidades.php", window.location.href);
+
+  // Lanza evento para comprobar la localidad
+  localidadUsuario.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      // Parametros de petición al servidor
+      let opcionesPeticion = {
+        method: "POST",
+        body: "localidadUsuario=" + localidadUsuario.value,
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+      };
+
+      cargarContenido(file, opcionesPeticion);
+    }
+  });
+}
+
+/** Carga de datos del servidor */
+async function cargarContenido(file, opcionesPeticion) {
+  let resultado = document.getElementById("resultado");
+  try {
+    let respuesta = await fetch(file, opcionesPeticion);
+    console.log(respuesta);
+    if (respuesta.ok) {
+      // Devuelve respuesta
+      let datosRespuesta = await respuesta.text();
+      // Tratamiento
+      if (datosRespuesta == "S") {
+        resultado.style.color = "green";
+        resultado.innerText = `La localidad ${localidadUsuario.value} está incluida`;
+      } else {
+        resultado.style.color = "red";
+        resultado.innerText = `La localidad ${localidadUsuario.value} NO está incluida`;
+      }
+    } else {
+      // Lanza error
+      throw new Error(respuesta.statusText);
+    }
+  } catch (error) {
+    // Error de red
+    console.error(error.message);
+  }
+}
